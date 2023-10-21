5da1fec517a850b495b422d6e65407fe0159cb31
DorianBadel <dorianbadel@protonmail.com>
2023-10-21 22:43:48 +0200

    Adding to BE, tutorial finished

---
+++ wishlistapp/app/wishlist/[id]/CreateWlItem.tsx
@@ -0,0 +1,61 @@
+"use client"; // Makes sure the component is only rendered on the client side
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function CreateWlItem() {
+  const [content, setContent] = useState({
+    Name: "",
+    Price: "",
+    Item_link: "",
+  });
+
+  const router = useRouter();
+
+  const create = async () => {
+    await fetch("http://127.0.0.1:8090/api/collections/WishlistItems/records", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        ...content,
+      }),
+    });
+
+    setContent({ Name: "", Price: "", Item_link: "" });
+
+    router.refresh();
+  };
+
+  return (
+    <form onSubmit={create}>
+      <h1>Create Wishlist Item</h1>
+      <input
+        type="text"
+        placeholder="Name"
+        onChange={(e) =>
+          setContent((prev) => ({ ...prev, Name: e.target.value }))
+        }
+        value={content.Name}
+      />
+      <input
+        type="text"
+        placeholder="Price"
+        onChange={(e) =>
+          setContent((prev) => ({ ...prev, Price: e.target.value }))
+        }
+        value={content.Price}
+      />
+      <input
+        type="text"
+        placeholder="Item_link"
+        onChange={(e) =>
+          setContent((prev) => ({ ...prev, Item_link: e.target.value }))
+        }
+        value={content.Item_link}
+      />
+      <button type="submit">Create note</button>
+    </form>
+  );
+}
--- wishlistapp/app/wishlist/page.tsx
+++ wishlistapp/app/wishlist/page.tsx
@@ -1,37 +1,43 @@
 import Link from "next/link";
+import CreateWlItem from "./[id]/CreateWlItem";
 
-async function getWishlistItems(){
-    const res = await fetch('http://127.0.0.1:8090/api/collections/WishlistItems/records?page=1&perPage=30', { cache: 'no-store' })
-    const data = await res.json();
+async function getWishlistItems() {
+  const res = await fetch(
+    "http://127.0.0.1:8090/api/collections/WishlistItems/records?page=1&perPage=30",
+    { cache: "no-store" }
+  );
+  const data = await res.json();
 
-    return data?.items as any[];
+  return data?.items as any[];
 }
 
 export default async function WishlistPage() {
-    const wishlistItems = await getWishlistItems();
+  const wishlistItems = await getWishlistItems();
 
-    return(
-        <div>
-            <h1>Wishlist Page</h1>
-            <div>
-                {wishlistItems?.map((item) => {
-                    return <WishlistItem key={item.id} wl_item={item}/>;
-                })}                
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Wishlist Page</h1>
+      <div>
+        {wishlistItems?.map((item) => {
+          return <WishlistItem key={item.id} wl_item={item} />;
+        })}
+      </div>
+
+      <CreateWlItem />
+    </div>
+  );
 }
 
 function WishlistItem({ wl_item }: any) {
-    const { id, Name, Price, Item_link } = wl_item || {};
-  
-    return (
-      <Link href={`/wishlist/${id}`}>
-        <div>
-          <h2>{Name}</h2>
-          <p>{Price}</p>
-          <p>{Item_link}</p>
-        </div>
-      </Link>
-    )
-  }
+  const { id, Name, Price, Item_link } = wl_item || {};
+
+  return (
+    <Link href={`/wishlist/${id}`}>
+      <div>
+        <h2>{Name}</h2>
+        <p>{Price}</p>
+        <p>{Item_link}</p>
+      </div>
+    </Link>
+  );
+}
